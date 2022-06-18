479e5350b56448cd0bf7d62b97705f352532629c
Kerim <Kerim@DESKTOP-JDQ2UQC>
2022-06-19 00:01:51 +0400

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿namespace ConsoleApp1;
+
+public class Program
+{
+    public static void Main()
+    {
+        using ModelContext context = new();
+
+        var GameModels = context.Model.ToList();
+
+        var newModel = new Model()
+        {
+            Style = GameStyle.Shooter,
+            GameName = "Fortnite"
+        };
+
+        GameModels.Add(newModel);
+
+        foreach (var item in GameModels)
+        {
+            Console.WriteLine($"{item.GameName}\n{item.Style}\n{item.ReleaseDate}");
+        }
+
+        context.SaveChanges();
+
+    }
+}
